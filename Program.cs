d208f6a3acf9d461e88189f7a74dfea903fa88bf
student 04 <stk_lum_04@outlook.de>
2022-03-16 08:28:51 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stunde_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            // Datentypen: ganze Zahlen; Dezimalzahlen, Zeichenketten, Zeichen,
+            //             Wahrheitswerte (Boolean)
+
+            // ganze Zahlen
+            int ganzeZahlA = 41;
+            int ganzeZahlB;
+            ganzeZahlB = 7;
+            short kleineGanzeZahl;
+            long großeGanzeZahl;
+
+
+            int ganzeZahlC = ganzeZahlA / ganzeZahlB;
+            Console.WriteLine(ganzeZahlC);
+
+            // Gleitkommazahlen (Dezimalzahlen)
+            double gleitkommaZahlA = 1.5;
+            double gleitkommaZahlB = 3;
+            decimal großeGleitkommazahl;
+
+            Console.WriteLine(gleitkommaZahlA / gleitkommaZahlB);
+
+            double gleichkommazahlC = ganzeZahlC + gleichkommaZahlA;
+            // int zu double -> kein Problem
+            // double zu int -> braucht Eingereifen
+            Console.WriteLine(ganzeZahlC);
+
+            bool wahrheitswert = true;
+            //später mehr
+
+            // Zeichen und Zeichenketten
+            string zeichenketteB = "Ja,";
+            string zeichenketteA = "Hallo Welt!";
+             string + string := Konkatenation;
+            Console.WriteLine(Konkatenation);
+#
+            H0llo
+        }
+    }
+}
+                        
